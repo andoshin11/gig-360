7518fee0b79550ef1b4faf2679418da3c79a990f
andoshin11 <shinglish11@gmail.com>
2017-06-17 20:22:52 +0900

    Install Browserify and Vue.js

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Gig360
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.browserify_rails.commandline_options = [
+      '-t [ babelify --presets [ latest ] --plugins [ add-module-exports ] ]',
+      '-t vueify',
+    ]
+    Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
